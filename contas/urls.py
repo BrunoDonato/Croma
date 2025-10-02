5d0e1d97560f8586095f247e751ad04c8b6c20cc
Bruno Amaral Carvalho Donato <brunoamaralsbt10@gmail.com>
2025-10-02 20:35:19 -0300

    feat: adicionado novos alertas claros de sessão expirada, acesso negado e erros de login/registro

--- contas/urls.py
+++ contas/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path("logout/", auth_views.LogoutView.as_view(next_page=reverse_lazy("contas:login")), name="logout"), # Logout, POST redireciona para a tela de login 
     path("dashboard/", views.dashboard, name="dashboard"),
     path("registrar/", views.registrar, name="registrar"),# Se usar o cadastro
-    path("admin-area/", views.admin_area, name="admin_area"),
+    path("admin-area/", views.admin_area, name="admin_area"), # Area restrita para administradores
 ]
